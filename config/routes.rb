3164370dee7d4299f03d857c49ac36626b4aed67
zekedran <saravanabalagi@hotmail.com>
2016-11-11 21:55:06 +0530

    Adding fb auth route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   post 'user_token' => 'user_token#create'
+  post '/auth/facebook/sign_up' => 'auth_facebook#get_details'
   resources :locations, :users
 end
