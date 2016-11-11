e82540f1746ac8eae880272f35ca38538ec85e5b
zekedran <saravanabalagi@hotmail.com>
2016-11-11 21:55:44 +0530

    Adding certificates due to SSL error

--- config/initializers/koala.rb
+++ config/initializers/koala.rb
@@ -1,5 +1,7 @@
 Koala::Facebook::OAuth.class_eval do
 
+  Koala.http_service.http_options = { :ssl => { :ca_file => Rails.root.join('lib/cacert.pem').to_s } }
+
   def initialize_with_default_settings(*args)
     raise "application id and/or secret are not specified in the envrionment" unless ENV['FACEBOOK_APP_ID'] && ENV['FACEBOOK_APP_SECRET']
     initialize_without_default_settings(ENV['FACEBOOK_APP_ID'].to_s, ENV['FACEBOOK_APP_SECRET'].to_s, args.first)
