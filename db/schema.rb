609fb698de352544777dfd6a82d4beabd3ef6976
zekedran <saravanabalagi@hotmail.com>
2016-11-12 16:53:53 +0530

    Adding verified_at timestamp to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161111170222) do
+ActiveRecord::Schema.define(version: 20161112111630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -341,6 +341,7 @@ ActiveRecord::Schema.define(version: 20161111170222) do
     t.datetime "updated_at",                   null: false
     t.string   "uid"
     t.string   "provider"
+    t.datetime "verified_at"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["mobile"], name: "index_users_on_mobile", unique: true, using: :btree
     t.index ["provider"], name: "index_users_on_provider", using: :btree
---
+++ test/controllers/verify_user_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class VerifyUserControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
