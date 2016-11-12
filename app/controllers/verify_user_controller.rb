9f7760138e1e7f5a11d3975c9d9b3a759e88b22c
zekedran <saravanabalagi@hotmail.com>
2016-11-12 19:08:35 +0530

    verifyUser now saves the timestamp if verified successfully

--- app/controllers/verify_user_controller.rb
+++ app/controllers/verify_user_controller.rb
@@ -3,19 +3,19 @@ class VerifyUserController < ApplicationController
   before_action :authenticate_user, :set_user
   # POST /auth/get_otp
   def get_otp
-    if MessagingService.send_otp? @user.mobile
+    if MessagingService.send_otp? @user
       render status: :ok, json: {status: 'otp_sent'}
     else
-      render status: :service_unavailable
+      render status: :service_unavailable, json: @user.errors
     end
   end
 
   # POST /auth/verify_otp
   def verify_otp
-    if MessagingService.verify_otp? @user.mobile, otp_params[:otp]
+    if MessagingService.verify_otp? @user, otp_params[:otp]
       render status: :ok, json: {status: 'verification_successful'}
     else
-      render status: :unprocessable_entity
+      render status: :unprocessable_entity, json: @user.errors
     end
   end
 
--- app/services/messaging_service.rb
+++ app/services/messaging_service.rb
@@ -1,28 +1,30 @@
 class MessagingService
 
   def self.faraday_msg91_post(api_path, json)
-    faraday = Faraday.new(:url => 'https://sendotp.msg91.com', :ssl => {:ca_file => Rails.root.join('lib/cacert.pem').to_s }) do |faraday|
-      faraday.response :logger, ::Logger.new(STDOUT), bodies: true
-      faraday.adapter Faraday.default_adapter
-    end
-    faraday.post do |req|
-              req.url api_path
-              req.headers['Content-Type'] = 'application/json'
-              req.headers['Application-Key'] = ENV['MSG91_APP_ID']
-              req.body = json.to_json
+    Faraday.new(:url => 'https://sendotp.msg91.com', :ssl => {:ca_file => Rails.root.join('lib/cacert.pem').to_s })
+                        .post do |req|
+                          req.url api_path
+                          req.headers['Content-Type'] = 'application/json'
+                          req.headers['Application-Key'] = ENV['MSG91_APP_ID']
+                          req.body = json.to_json
     end
   end
 
-  def self.send_otp?(mobile_number)
+  def self.send_otp?(user)
     status = false
     begin
       response = faraday_msg91_post('/api/generateOTP',{
             countryCode: '91',
-            mobileNumber: mobile_number,
+            mobileNumber: user.mobile,
             getGeneratedOtp: true
         })
       json = response.body && response.body.length >= 2 ? JSON.parse(response.body) : nil
-      status = true if json['status'] == 'success' && json['response']['code'] == 'OTP_SENT_SUCCESSFULLY'
+      if json['status'] == 'success' && json['response']['code'] == 'OTP_SENT_SUCCESSFULLY'
+        status = true
+      else
+        user.errors.clear
+        user.errors.add(:otp, json['response']['code'].humanize)
+      end
     rescue => exception
       puts 'Exception thrown during send_otp: ' + exception.class.name
       puts exception
@@ -31,16 +33,22 @@ class MessagingService
     end
   end
 
-  def self.verify_otp?(mobile_number, otp)
+  def self.verify_otp?(user, otp)
     status = false
     begin
       response = faraday_msg91_post('/api/verifyOTP', {
             countryCode: '91',
-            mobileNumber: mobile_number,
+            mobileNumber: user.mobile,
             oneTimePassword: otp
         })
       json = response.body && response.body.length >= 2 ? JSON.parse(response.body) : nil
-      status = true if json['status'] == 'success' && json['response']['code'] == 'NUMBER_VERIFIED_SUCCESSFULLY'
+      if json['status'] == 'success' && json['response']['code'] == 'NUMBER_VERIFIED_SUCCESSFULLY'
+        user.update_attribute(:verified_at, response.headers['date'])
+        status = true
+      else
+        user.errors.clear
+        user.errors.add(:otp, json['response']['code'].humanize)
+      end
     rescue => exception
       puts 'Exception thrown during verify_otp: ' + exception.class.name
       puts exception
