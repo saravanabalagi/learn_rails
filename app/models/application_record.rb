3ee71fd66267a99778c579ce4cfead376ad1c801
Saravanabalagi R <saravanabalagi@hotmail.com>
2016-11-24 14:17:53 +0530

    Stripping created_at and updated_at

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,10 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def serializable_hash(options={})
+    options[:except] ||= [:created_at, :updated_at] - Array(options[:include])
+    options[:include] = Array(options[:include]) - [:created_at, :updated_at]
+    super(options)
+  end
+
 end
