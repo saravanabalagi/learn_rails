ba93157ab0906d14540f348fcd6db9d9312edaab
Saravanabalagi R <saravanabalagi@hotmail.com>
2016-11-25 23:05:32 +0530

    Render dishes altered

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,14 +1,9 @@
 class DishesController < ApplicationController
-  # GET /dishes
-  def index
-    @dishes = Dish.all
-
-    render json: @dishes
-  end
-
   # GET /dishes/1
   def show
     @dish = Dish.find(params[:id])
-    render json: @dish
+    render json: @dish, include: { dish_variants:
+                                       { include: { add_on_type_links:
+                                                        { include: :add_on_links }} }}
   end
 end
