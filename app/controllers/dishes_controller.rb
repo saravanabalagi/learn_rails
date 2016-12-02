46db84fdd3cdb21991ee51089b01e10756cebb01
Saravanabalagi R <saravanabalagi@hotmail.com>
2016-12-03 01:16:20 +0530

    Including min price in dishes

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -4,6 +4,7 @@ class DishesController < ApplicationController
     @dish = Dish.find(params[:id])
     render json: @dish, include: { dish_variants:
                                        { include: { add_on_type_links:
-                                                        { include: :add_on_links }} }}
+                                                        { include: :add_on_links }} },
+                                    restaurant: {only: [:id, :name, :logo]} },  methods: [:price]
   end
 end
