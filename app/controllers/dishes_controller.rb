3164b89f6e83e17e11cf6c433a183e6bfa59e25d
Zeke <saravanabalagi@hotmail.com>
2017-01-20 13:17:45 +0530

    Rendering full dish will all addons

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -5,10 +5,62 @@ class DishesController < ApplicationController
   def show
     @dish = @dishes.find(params[:id])
     if @dish.present?
-      render json: @dish, include: { dish_variants:
-                                       { include: { add_on_type_links:
-                                                        { include: :add_on_links }} },
-                                    restaurant: {only: [:id, :name, :logo]} },  methods: [:price]
+      render json: @dish, include: {
+                              dish_variants: {
+                                   only: [:id, :price, :image, :icon],
+                                   include: {
+                                       add_on_type_links: {
+                                           only: [:id, :min, :max],
+                                           include: { add_on_links: {
+                                               only: [:id, :price, :selected],
+                                               include: {
+                                                   add_on: {
+                                                       only: [:id, :name]
+                                                   }
+                                               }
+                                           }, add_on_type: {only: [:id, :name]}}
+                                       },
+                                      variant: {
+                                        only: [:display_name],
+                                        include: {
+                                            variant_category: {
+                                              only: :name,
+                                              include: { add_on_type_links: {
+                                                  only: [:id, :min, :max],
+                                                  include: { add_on_links: {
+                                                      only: [:id, :price, :selected],
+                                                      include: {
+                                                          add_on: {
+                                                              only: [:id, :name]
+                                                          }
+                                                      }
+                                                  }, add_on_type: {only: [:id, :name]}}
+                                              }}
+                                            },
+                                            add_on_type_links: {
+                                                only: [:id, :min, :max],
+                                                include: { add_on_links: {
+                                                    only: [:id, :price, :selected],
+                                                    include: {
+                                                        add_on: {
+                                                            only: [:id, :name]
+                                                        }
+                                                    }
+                                                }, add_on_type: {only: [:id, :name]}}
+                                            }}
+                                      },
+                                      food_label: { only: :name}
+                                   }
+                              },
+                              restaurant: {
+                                            only: [:id, :name, :logo],
+                                            include: { brand: {
+                                                only: [:id, :name, :logo]
+                                            }}
+                              }
+                          },
+                          only: [:id, :name, :image],
+                          methods: [:price]
     else
       render status: :not_found
     end
