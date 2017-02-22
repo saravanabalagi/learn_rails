f91bc85dd87114fc633a02e9a4763aab52f24ba1
Zeke <saravanabalagi@hotmail.com>
2017-02-23 01:10:38 +0530

    Generate uniq and random pickup_code

--- app/models/restaurant_order.rb
+++ app/models/restaurant_order.rb
@@ -7,4 +7,14 @@ class RestaurantOrder < ApplicationRecord
   has_many :order_items, :dependent => :destroy
   accepts_nested_attributes_for :order_items, allow_destroy: true
 
+  before_create :set_pickup_code
+
+  private
+  def set_pickup_code
+    o = [('0'..'9'), ('A'..'Z')].map(&:to_a).flatten
+    begin
+      self.pickup_code = (0...7).map { o[rand(o.length)] }.join
+    end while self.class.exists?(pickup_code: self.pickup_code)
+  end
+
 end
